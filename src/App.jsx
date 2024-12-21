4c9ab911a6b75a8a542aaa1e5ac54410a7b0fc2a
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2024-12-21 10:33:21 +0530

    added order now button

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import IconArrowRight from './Components/Icons/ArrowRight'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        {/* Hero Section */}
+        <section
+        className='flex flex-col-reverse items-center justify-center py-5 md:flex-row md:gap-7 bg-gradient-to-r from-amber-50 to-orange-300'>
+          <div className='w-4/6 ml-4 md:w-2/6 md:text-left' >
+             <div className='flex justify-center text-4xl md:justify-normal'>
+                 <h1 className=' font-bold text-4xl text-transparent  bg-gradient-to-r from-orange-500 to-orange-300 bg-clip-text'>
+                   Enjoy the Slice
+                 </h1>
+                 <h1>
+                    😋
+                 </h1>
+             </div>
+             <p className='pb-4 text-[#6B7280]'>
+              The Pizza app lets you order your favourite pizza from the comfort of your home. Enjoy the best Pizza in the town with just a few clicks.
+             </p>
+             <button
+             className=' flex items-center px-4 py-2 text-white bg-yellow-500 rounded-md hover:bg-yellow-600 group'>
+              Order Now
+              <span className='inline-block ml-3  transition-transform ease-in-out group-hover:translate-x-2'>
+                 <IconArrowRight/>
+              </span>
+             </button>
+          </div>
+        </section>
+    </div>
   )
 }
 
