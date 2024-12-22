cb84c6ddb882889f423923d8448d0f7459e70d85
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2024-12-22 13:15:04 +0530

    Added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import './App.css';
 import Home from './Pages/Home'
 import { Route, Routes } from 'react-router-dom';
 import Signup from './Pages/Auth/Signup';
-import Login from './Pages/Auth/Login.jsx'
+import Login from './Pages/Auth/Login.jsx';
+import NotFound from './Pages/NotFound'
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/auth/signup' element={<Signup />} />
         <Route path='/auth/login' element={<Login />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
