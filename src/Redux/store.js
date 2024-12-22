a9dfd3e2b50280f2975d853ea208beb9ee6a21e7
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2024-12-22 15:36:05 +0530

    added first slice as auth

---
+++ src/Redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import AuthSliceReducer from "./Slices/AuthSlice";
+export const store = configureStore({
+    reducer: {
+        auth: AuthSliceReducer
+    },
+    devTools: true
+});
