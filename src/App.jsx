b5b4047e043f2a0fa99d9c5688be59ce0d0e9781
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2024-12-22 15:44:36 +0530

    Added presentation layer for add product component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Home from './Pages/Home'
 import { Route, Routes } from 'react-router-dom';
 import Signup from './Pages/Auth/Signup';
 import Login from './Pages/Auth/Login.jsx';
-import NotFound from './Pages/NotFound'
+import NotFound from './Pages/NotFound';
+
+import Denied from './Pages/Denied'
+import AddProduct from './Pages/Admin/Addproduct'
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/denied" element={<Denied />} />
         <Route path='/auth/signup' element={<Signup />} />
         <Route path='/auth/login' element={<Login />} />
+        <Route path='/admin/addProduct' element={<AddProduct />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </>
